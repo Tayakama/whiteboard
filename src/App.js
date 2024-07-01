d00877b4dca476e50be36a54c3bf0e533dffa01c
Tayakama <tayakama@gmail.com>
2024-07-01 11:44:14 +0900

    Fix App.js and project structure

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Online Whiteboard</h1>
+      {/* ここにホワイトボードのコンポーネントを追加します */}
+    </div>
+  );
+}
+
+export default App;
